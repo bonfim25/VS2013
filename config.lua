7603b372f4526ee1372b5818e5cfbbfd59745aa9
bonfim25 <jose.doido3@hotmail.com>
2017-04-02 00:04:29 -0300

    Initial

---
+++ config.lua
@@ -0,0 +1,298 @@
+	accountManager = false
+	namelockManager = false
+	newPlayerChooseVoc = false
+	newPlayerSpawnPosX = 32097
+	newPlayerSpawnPosY = 32219
+	newPlayerSpawnPosZ = 7
+	newPlayerTownId = 6
+	newPlayerLevel = 1
+	newPlayerMagicLevel = 1
+	generateAccountNumber = false
+	generateAccountSalt = false
+
+	useFragHandler = true
+	redSkullLength = 30 * 24 * 60 * 60
+	blackSkullLength = 45 * 24 * 60 * 60
+	dailyFragsToRedSkull = 6
+	weeklyFragsToRedSkull = 18
+	monthlyFragsToRedSkull = 30
+	dailyFragsToBlackSkull = dailyFragsToRedSkull
+	weeklyFragsToBlackSkull = weeklyFragsToRedSkull
+	monthlyFragsToBlackSkull = monthlyFragsToRedSkull
+	dailyFragsToBanishment = dailyFragsToRedSkull
+	weeklyFragsToBanishment = weeklyFragsToRedSkull
+	monthlyFragsToBanishment = monthlyFragsToRedSkull
+	blackSkulledDeathHealth = 100
+	blackSkulledDeathMana = 50
+	useBlackSkull = true
+	advancedFragList = true
+
+	notationsToBan = 3
+	warningsToFinalBan = 4
+	warningsToDeletion = 5
+	banLength = 7 * 24 * 60 * 60
+	killsBanLength = 7 * 24 * 60 * 60
+	finalBanLength = 30 * 24 * 60 * 60
+	ipBanishmentLength = 1 * 24 * 60 * 60
+	broadcastBanishments = true
+	maxViolationCommentSize = 200
+	violationNameReportActionType = 2
+	autoBanishUnknownBytes = false
+
+	worldType = "pvp"
+	protectionLevel = 30
+	pvpTileIgnoreLevelAndVocationProtection = true
+	pzLocked = 25 * 1000
+	huntingDuration = 40 * 1000
+	criticalHitChance = 7
+	criticalHitMultiplier = 1
+	displayCriticalHitNotify = true
+	removeWeaponAmmunition = true
+	removeWeaponCharges = true
+	removeRuneCharges = true
+	whiteSkullTime = 15 * 60 * 1000
+	noDamageToSameLookfeet = false
+	showHealingDamage = true
+	showHealingDamageForMonsters = false
+	fieldOwnershipDuration = 5 * 1000
+	stopAttackingAtExit = false
+	loginProtectionPeriod = 10 * 1000
+	deathLosePercent = -1
+	stairhopDelay = 2 * 1000
+	pushCreatureDelay = 2 * 1000
+	deathContainerId = 1988
+	gainExperienceColor = 215
+	addManaSpentInPvPZone = true
+	squareColor = 0
+    noDamageToPartyMembers = true
+	allowFightback = true
+	fistBaseAttack = 7
+
+	worldId = 0
+	ip = "otvalar.no-ip.biz"
+	bindOnlyGlobalAddress = false
+	loginPort = 7171
+	gamePort = 7172
+	loginTries = 10
+	retryTimeout = 5 * 1000
+	loginTimeout = 60 * 1000
+	maxPlayers = 1000
+	motd = "Welcome to the Valar."
+	displayOnOrOffAtCharlist = false
+	onePlayerOnlinePerAccount = true
+	allowClones = false
+	allowWalkthrough = false
+	serverName = "Valar"
+	loginMessage = "Welcome to the Valar, chose you character!!!"
+	statusTimeout = 5 * 60 * 1000
+	replaceKickOnLogin = true
+	forceSlowConnectionsToDisconnect = false
+	loginOnlyWithLoginServer = false
+	premiumPlayerSkipWaitList = false
+	maxPacketsPerSecond = 100
+
+	rsaPrime1 = "14299623962416399520070177382898895550795403345466153217470516082934737582776038882967213386204600674145392845853859217990626450972452084065728686565928113"
+	rsaPrime2 = "7630979195970404721891201847792002125535401292779123937207447574596692788513647179235335529307251350570728407373705564708871762033017096809910315212884101"
+	rsaPublic = "65537"
+	rsaModulus = "109120132967399429278860960508995541528237502902798129123468757937266291492576446330739696001110603907230888610072655818825358503429057592827629436413108566029093628212635953836686562675849720620786279431090218017681061521755056710823876476444260558147179707119674283982419152118103759076030616683978566631413"
+	rsaPrivate = "46730330223584118622160180015036832148732986808519344675210555262940258739805766860224610646919605860206328024326703361630109888417839241959507572247284807035235569619173792292786907845791904955103601652822519121908367187885509270025388641700821735345222087940578381210879116823013776808975766851829020659073"
+
+	mysqlHost = "127.0.0.1"
+	mysqlUser = "root"
+	mysqlPass = "j92386875"
+	mysqlDatabase = "valar"
+	mysqlPort = 3306
+	passwordType = "sha1"
+
+	deathListEnabled = true
+	deathListRequiredTime = 1 * 60 * 1000
+	deathAssistCount = 19
+	maxDeathRecords = 5
+
+	ingameGuildManagement = false
+	levelToFormGuild = 50
+	premiumDaysToFormGuild = 0
+	guildNameMinLength = 4
+	guildNameMaxLength = 20
+
+	highscoreDisplayPlayers = 15
+	updateHighscoresAfterMinutes = 20
+
+	buyableAndSellableHouses = true
+	houseNeedPremium = true
+	bedsRequirePremium = false
+	levelToBuyHouse = 20
+	housesPerAccount = 1
+	houseRentAsPrice = false
+	housePriceAsRent = false
+	housePriceEachSquare = 900
+	houseRentPeriod = "never"
+	houseCleanOld = 0
+	guildHalls = false
+
+	timeBetweenActions = 200
+	timeBetweenExActions = 1000
+	hotkeyAimbotEnabled = true
+
+	enableCast = true
+
+	mapName = "realglobal"
+	mapAuthor = "desconhecido"
+	randomizeTiles = true
+	storeTrash = true
+	cleanProtectedZones = true
+	mailboxDisabledTowns = ""
+
+	marketOfferDuration = 30 * 24 * 60 * 60
+	premiumToCreateMarketOffer = true
+	checkExpiredMarketOffersEachMinutes = 60
+	maxMarketOffersAtATimePerPlayer = 100
+
+	defaultPriority = "high"
+	niceLevel = 5
+	coresUsed = "-1"
+
+	startupDatabaseOptimization = true
+	updatePremiumStateAtStartup = true
+	confirmOutdatedVersion = false
+
+	formulaLevel = 5.0
+	formulaMagic = 1.0
+	bufferMutedOnSpellFailure = false
+	spellNameInsteadOfWords = true
+	emoteSpells = true
+	unifiedSpells = true
+	classicEquipmentSlots = true
+
+	allowDualWielding = true
+	dualWieldingSpeedRate = 200
+	dualWieldingDamageRate = 100
+
+	allowChangeOutfit = true
+	allowChangeColors = true
+	allowChangeAddons = true
+	disableOutfitsForPrivilegedPlayers = false
+	addonsOnlyPremium = false
+
+	dataDirectory = "data/"
+	logsDirectory = "data/logs/"
+	bankSystem = true
+	displaySkillLevelOnAdvance = false
+	promptExceptionTracerErrorBox = true
+	maximumDoorLevel = 500
+	maxMessageBuffer = 4
+	tradeLimit = 100
+
+	separateVipListPerCharacter = false
+	vipListDefaultLimit = 20
+	vipListDefaultPremiumLimit = 100
+
+	saveGlobalStorage = true
+	useHouseDataStorage = false
+	storePlayerDirection = false
+
+	checkCorpseOwner = true
+	monsterLootMessage = 3
+	monsterLootMessageType = 25
+
+	ghostModeInvisibleEffect = false
+	ghostModeSpellEffects = true
+
+	idleWarningTime = 14 * 60 * 1000
+	idleKickTime = 15 * 60 * 1000
+	reportsExpirationAfterReads = 1
+	playerQueryDeepness = 2
+	tileLimit = 0
+	protectionTileLimit = 0
+	houseTileLimit = 0
+
+	freePremium = false
+	premiumForPromotion = false
+
+	blessings = true
+	blessingOnlyPremium = true
+	blessingReductionBase = 30
+	blessingReductionDecrement = 5
+	eachBlessReduction = 8
+
+	experienceStages = true
+	rateExperience = 10.0
+	rateExperienceFromPlayers = 0
+	rateSkill = 2.0
+	rateMagic = 3.0
+	rateLoot = 2.0
+	rateSpawn = 1
+
+	rateMonsterHealth = 1.3
+	rateMonsterMana = 1.0
+	rateMonsterAttack = 1.1
+	rateMonsterDefense = 1.4
+
+	minLevelThresholdForKilledPlayer = 0.9
+	maxLevelThresholdForKilledPlayer = 1.1
+
+	rateStaminaLoss = 1
+	rateStaminaGain = 3
+	rateStaminaThresholdGain = 12
+	staminaRatingLimitTop = 40 * 60
+	staminaRatingLimitBottom = 14 * 60
+	staminaLootLimit = 14 * 60
+	rateStaminaAboveNormal = 1.5
+	rateStaminaUnderNormal = 0.5
+	staminaThresholdOnlyPremium = true
+
+	experienceShareRadiusX = 30
+	experienceShareRadiusY = 30
+	experienceShareRadiusZ = 1
+	experienceShareLevelDifference = 2 / 3
+	extraPartyExperienceLimit = 20
+	extraPartyExperiencePercent = 50
+	experienceShareActivity = 2 * 60 * 1000
+
+	globalSaveEnabled = true
+	globalSaveHour = 24
+	globalSaveMinute = 0
+	shutdownAtGlobalSave = true
+	cleanMapAtGlobalSave = true
+
+	deSpawnRange = 2
+	deSpawnRadius = 50
+
+	maxPlayerSummons = 3
+	teleportAllSummons = true
+	teleportPlayerSummons = true
+
+	statusPort = 7171
+	ownerName = ""
+	ownerEmail = "@otland.net"
+	url = "http://otland.net/"
+	location = "Europe"
+	displayGamemastersWithOnlineCommand = false
+
+	displayPlayersLogging = true
+	prefixChannelLogs = ""
+	runFile = ""
+	outputLog = ""
+	truncateLogOnStartup = false
+
+	managerPort = 7171
+	managerLogs = true
+	managerPassword = ""
+	managerLocalhostOnly = true
+	managerConnectionsLimit = 1
+
+	adminPort = 7171
+	adminLogs = true
+	adminPassword = ""
+	adminLocalhostOnly = true
+	adminConnectionsLimit = 1
+	adminRequireLogin = true
+	adminEncryption = ""
+	adminEncryptionData = ""
+
+	warnUnsafeScripts = true
+	convertUnsafeScripts = true
+
+
+	defaultPriority = "high"
+	startupDatabaseOptimization = true
